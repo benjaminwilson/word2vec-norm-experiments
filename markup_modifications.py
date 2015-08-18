f38ed388156ce9cf2f33143e2dc79d5e4ec8a081
benjamin wilson <benjamin@asmusas.net>
2015-08-18 10:57:04 +0200

    generate three sample texts for the article

--- markup_modifications.py
+++ markup_modifications.py
@@ -2,51 +2,64 @@
 Reads a sample text from stdin and writes to stdout LaTeX figures, with
 highlighting, showing the corpus modifications made for the two experiments
 with the modifications highlighted in red.
-"""
-import sys
+""" #FIXME
 import re
 from StringIO import StringIO
 from parameters import *
 from functions import *
 
-def markup(raw_output):
-    hl = re.sub(r'([A-Z]+_[0-9]+)', r'\\textcolor{red}{\1}', raw_output)
-    return re.sub('_', '\_', hl)
+def write_markup(raw_output, f_out):
+    """
+    Markup all modifications to a sample text for typesetting in LaTeX.
+    """
+    f_out.write(r"\texttt{")
+    output = re.sub(r'([A-Z]+_[0-9]+)', r'\\textcolor{red}{\1}', raw_output)
+    output = re.sub('_', '\_', output)
+    output = r'\newline '.join(output.strip().split('\n'))
+    f_out.write(output)
+    f_out.write(r"}")
 
-input_text = sys.stdin.read()
-counts = count_words(StringIO(input_text))
-total_words = sum(counts.values())
+# PERFORM THE REPLACEMENT PROCEDURE FOR THE WORD FREQUENCY EXPERIMENT FOR THE WORD "CAT"
 
-# intersperse the meaningless token throughout the corpus
-w_meaningless = StringIO()
-intersperse_words({meaningless_token: meaningless_token_frequency}, StringIO(input_text), w_meaningless)
+word = 'cat'
+word_samplers = {word: truncated_geometric_sampling(word, word_freq_experiment_ratio, word_freq_experiment_power_max)}
 
-# perform the replacement procedures for the word frequency experiment
-word_samplers = {}
-for word in ['cat', meaningless_token]:
-    word_samplers[word] = truncated_geometric_sampling(word, word_freq_experiment_ratio, word_freq_experiment_power_max)
+tmp_file = StringIO()
+with file('article/sample_input') as f_in:
+    replace_words(word_samplers, f_in, tmp_file)
 
-w_replacement = StringIO()
-replace_words(word_samplers, StringIO(w_meaningless.getvalue()), w_replacement)
+with file('article/word-frequency-experiment-text-cat.tex', 'w') as f_out:
+    write_markup(tmp_file.getvalue(), f_out)
 
-print r"""\begin{figure}[t]
-	\texttt{"""
-print markup(w_replacement.getvalue()).strip()
-print r"""}
-\caption{Text modified for the word frequency experiment, where the word \word{cat} was chosen, $\lambda=0.5$ and $n=20$.}
-\label{fig:word-frequency-experiment-text}
-\end{figure}"""
+# PERFORM THE REPLACEMENT PROCEDURE FOR THE WORD FREQUENCY EXPERIMENT FOR THE MEANINGLESS WORD
 
-print '-' * 80
+word_samplers = {meaningless_token: truncated_geometric_sampling(word, word_freq_experiment_ratio, word_freq_experiment_power_max)}
 
-# cooccurrence noise experiment
+# intersperse the meaningless token
+tmp_file1 = StringIO()
+with file('article/sample_input') as f_in:
+    intersperse_words({meaningless_token: meaningless_token_frequency}, f_in, tmp_file1)
+tmp_file1.seek(0)
+
+tmp_file2 = StringIO()
+replace_words(word_samplers, tmp_file1, tmp_file2)
+
+with file('article/word-frequency-experiment-text-void.tex', 'w') as f_out:
+    write_markup(tmp_file2.getvalue(), f_out)
+
+# COOCCURRENCE NOISE EXPERIMENT
 coocc_noise_experiment_power_max = 3 # better for the example, be sure to note in caption
 word = 'cat'
-word_samplers = {}
-word_samplers[word] = truncated_geometric_sampling(word, coocc_noise_experiment_ratio, coocc_noise_experiment_power_max)
+word_samplers = {word: truncated_geometric_sampling(word, coocc_noise_experiment_ratio, coocc_noise_experiment_power_max)}
 
-w_replacement = StringIO()
-replace_words(word_samplers, StringIO(input_text), w_replacement)
+with file('article/sample_input') as f_in:
+    counts = count_words(f_in)
+total_words = sum(counts.values())
+
+tmp_file1 = StringIO()
+with file('article/sample_input') as f_in:
+    replace_words(word_samplers, f_in, tmp_file1)
+tmp_file1.seek(0)
 
 # add noise to the cooccurrence distributions of experiment 2 words
 token_freq_dict = dict()
@@ -55,13 +68,8 @@ for i in range(1, coocc_noise_experiment_power_max + 1):
     current_freq = target_freq * truncated_geometric_proba(coocc_noise_experiment_ratio, i, coocc_noise_experiment_power_max)
     token_freq_dict[build_experiment_token(word, i)] = target_freq - current_freq
 
-w_noise = StringIO()
-intersperse_words(token_freq_dict, StringIO(w_replacement.getvalue()), w_noise)
+tmp_file2 = StringIO()
+intersperse_words(token_freq_dict, tmp_file1, tmp_file2)
 
-print r"""\begin{figure}[t]
-	\texttt{"""
-print markup(w_noise.getvalue()).strip()
-print r"""}
-\caption{Text modified for the co-occurrence noise experiment, where the word \word{cat} was chosen, $\lambda = 5/6$ and $n=3$.}
-\label{fig:cooccurrence-noise-experiment-text}
-\end{figure}"""
+with file('article/cooccurrence-noise-experiment.tex', 'w') as f_out:
+    write_markup(tmp_file2.getvalue(), f_out)
